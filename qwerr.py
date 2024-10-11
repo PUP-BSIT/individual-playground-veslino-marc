8a4ecbc53f7bdd2f501c4c0b4b34f5488b4c7868
veslino-marc <veslinomarc4@gmail.com>
2024-10-12 07:42:26 +0800

    Sample python

---
+++ qwerr.py
@@ -0,0 +1,7 @@
+num1 = int (input ("Enter the First Number: "))
+num2 = int (input ("Enter the Second Number: "))
+
+if num1 == num2: 
+    print ("The two numbers are equal")
+else:
+    print ("The two number are not equal")
